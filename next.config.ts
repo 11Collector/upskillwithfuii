6fd9895ebb2f63782f8931aaf04c7fa6161bee9b
11Collector <emotion.tuii@gmail.com>
2026-07-15 21:28:52 +0700

    fix(security): complete CSP configuration whitelisting analytics, textures, and OAuth domains

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://*.firebaseapp.com https://js.stripe.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://www.googletagmanager.com https://*.firebaseapp.com https://js.stripe.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.googleusercontent.com https://ssl.gstatic.com https://firebasestorage.googleapis.com https://*.stripe.com;
+  img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.googleusercontent.com https://ssl.gstatic.com https://www.gstatic.com https://www.transparenttextures.com https://www.google-analytics.com https://*.google-analytics.com https://firebasestorage.googleapis.com https://*.stripe.com;
   font-src 'self' data: https://fonts.gstatic.com;
-  connect-src 'self' https://*.googleapis.com wss://*.firebaseio.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://api.stripe.com;
+  connect-src 'self' data: https://*.googleapis.com wss://*.firebaseio.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://*.google-analytics.com https://*.analytics.google.com https://api.stripe.com;
   frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://js.stripe.com https://hooks.stripe.com;
   media-src 'self' data: blob:;
   object-src 'none';
