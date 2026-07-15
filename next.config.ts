05b474ad9bd95b00ff9e32f65fb582e1fbc0ab84
11Collector <emotion.tuii@gmail.com>
2026-07-15 21:23:31 +0700

    fix(security): whitelist Google Sign-in and Firebase Auth domains in CSP

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://js.stripe.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://*.firebaseapp.com https://js.stripe.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: blob: https://lh3.googleusercontent.com https://firebasestorage.googleapis.com https://*.stripe.com;
+  img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.googleusercontent.com https://ssl.gstatic.com https://firebasestorage.googleapis.com https://*.stripe.com;
   font-src 'self' data: https://fonts.gstatic.com;
-  connect-src 'self' https://*.googleapis.com wss://*.firebaseio.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://api.stripe.com;
-  frame-src 'self' https://js.stripe.com https://hooks.stripe.com;
+  connect-src 'self' https://*.googleapis.com wss://*.firebaseio.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://accounts.google.com https://*.firebaseapp.com https://api.stripe.com;
+  frame-src 'self' https://accounts.google.com https://*.firebaseapp.com https://js.stripe.com https://hooks.stripe.com;
   media-src 'self' data: blob:;
   object-src 'none';
   base-uri 'self';
